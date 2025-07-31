43cf43914cc382ea2b2a5f0da45b4c5c42e5e905
annabeldu111 <durain025@gmail.com>
2025-07-31 15:30:26 -0400

    update

--- src/fama_macbeth_package.py
+++ src/fama_macbeth_package.py
@@ -1,85 +1,90 @@
 import pandas as pd
 import numpy as np
-import statsmodels.api as sm
+from linearmodels.panel import FamaMacBeth
+from linearmodels import OLS
 from scipy import stats
-from typing import Optional
+import statsmodels.api as sm
 
-class FamaMacBethRolling:
+class FamaMacBethRollingLM:
     def __init__(
         self,
-        dependent: pd.DataFrame,  # y: index date, cols ticker, stk excess rtn (r-rf)
-        exog: pd.DataFrame,       # x: index date, cols factors, fct rtn
-        window: Optional[int] = None, # default to be None
+        dependent: pd.DataFrame,  # y: (dates × tickers)
+        factors: pd.DataFrame,    # factor returns: (dates × factors)
+        window: int,
     ):
         self.y = dependent
-        self.x = exog
+        self.factors = factors
         self.window = window
 
     def fit(self, cov_type="robust"):
-        y, x = self.y, self.x
-        dates = y.index
-        tickers = y.columns
+        dates = self.y.index
+        tickers = self.y.columns
+        factor_names = self.factors.columns
 
         betas_rolling = {}
-        lambdas = []
+        lambda_list = []
+        lambda_dates = []
 
-        # Time Series regression, cal rolling beta
+        # Time-Series regression to calculate rolling betas
         for i in range(self.window, len(dates)):
             end_date = dates[i]
-            
-            # check if window is set
-            if self.window is None:
-                start_date = dates[0]
-                y_window = y.loc[start_date:end_date] # directly use the past data
-                x_window = x.loc[start_date:end_date]
-            else:
-                if i < self.window:
-                    continue
-                start_date = dates[i - self.window]
-                y_window = y.loc[start_date:end_date]
-                x_window = x.loc[start_date:end_date]
+            start_date = dates[i - self.window]
+            y_window = self.y.loc[start_date:end_date]
+            factors_window = self.factors.loc[start_date:end_date]
 
-            betas_period = pd.DataFrame(index=tickers, columns=x.columns, dtype=float)
+            betas_period = pd.DataFrame(index=tickers, columns=factor_names, dtype=float)
 
             for ticker in tickers:
                 y_stock = y_window[ticker].dropna()
-                common_dates = y_stock.index.intersection(x_window.index)
+                common_dates = y_stock.index.intersection(factors_window.index)
                 y_stock = y_stock.loc[common_dates]
-                x_fct = x_window.loc[common_dates]
+                x_fct = factors_window.loc[common_dates]
 
                 if len(y_stock) < self.window:
                     continue
 
                 x_fct_const = sm.add_constant(x_fct)
-                model = sm.OLS(y_stock, x_fct_const).fit()
+                model = OLS(y_stock, x_fct_const).fit()
                 betas_period.loc[ticker] = model.params[1:]  # drop constant
 
             betas_rolling[end_date] = betas_period
 
-        # Cross-sectional regression
-        lambda_list = []
-        lambda_dates = []
-
+        # Cross-sectional regression (Fama-MacBeth)
         for date in dates[self.window:]:
             betas_period = betas_rolling[date]
-            y_cross = y.loc[date].dropna()
+            y_cross = self.y.loc[date].dropna()
             valid_tickers = y_cross.index.intersection(betas_period.dropna().index)
 
+            K = betas_period.shape[1]
+            
             if len(valid_tickers) == 0:
+                continue
+            
+            if len(valid_tickers) <= K:
                 continue
 
             y_cross = y_cross.loc[valid_tickers]
             beta_cross = betas_period.loc[valid_tickers]
 
-            beta_cross_const = sm.add_constant(beta_cross)
-            model = sm.OLS(y_cross, beta_cross_const).fit()
+            y_panel = y_cross.rename_axis('ticker').to_frame(name='excess_return')
+            y_panel['date'] = date
+            y_panel = y_panel.reset_index().set_index(['ticker', 'date'])
 
-            lambda_list.append(model.params[1:])
+            beta_panel = beta_cross.copy()
+            beta_panel['date'] = date
+            beta_panel = beta_panel.reset_index().set_index(['ticker', 'date'])
+
+            # Call FamaMacBeth
+            fm = FamaMacBeth(y_panel['excess_return'], beta_panel)
+            res = fm.fit(cov_type=cov_type)
+
+            lambda_list.append(res.params)
             lambda_dates.append(date)
 
+        # lambda time series results
         lambdas = pd.DataFrame(lambda_list, index=lambda_dates)
 
-        # lambda stats, mean, cov, stderror, tstat, pvalue
+        # lambda stats
         lambda_mean = lambdas.mean()
         lambda_cov = lambdas.cov()
         lambda_se = np.sqrt(np.diag(lambda_cov / len(lambdas)))
@@ -94,29 +99,26 @@ class FamaMacBethRolling:
             "p_values": lambda_pvals,
             "cov": lambda_cov / len(lambdas),
             "factor_premiums_timeseries": lambdas,
-            "rolling_betas": betas_rolling,
         }
 
         return results
 
-
-def shanken_correction(results: dict, threshold_t_over_n=3, tstat_threshold=3.0):
+def shanken_correction_LM(results: dict, threshold_t_over_n=3, tstat_threshold=3.0):
     lambdas = results["factor_premiums_timeseries"]
     
-    # Concatenate betas across all periods vertically
+    # Concatenate all betas vertically
     betas_all = pd.concat(results["rolling_betas"].values(), axis=0).dropna()
 
-    T = lambdas.shape[0]  # num of timeperiods
-    N = betas_all.shape[0]  # num of β = tickeramount * T
+    T = lambdas.shape[0]  # periods
+    N = betas_all.shape[0]  # num of betas (ticker-periods)
     
-    lambda_mean = lambdas.mean().values.reshape(-1,1)  # (K × 1)
-    Sigma_lambda = lambdas.cov().values                # (K × K)
-    Sigma_beta = betas_all.cov().values                # (K × K)
+    lambda_mean = lambdas.mean().values.reshape(-1, 1)
+    Sigma_lambda = lambdas.cov().values
+    Sigma_beta = betas_all.cov().values
 
     std_err = results["std_err"]
     t_stats = results["t_stats"]
 
-    # condition to apply shanken correction
     apply_shanken = (T/N < threshold_t_over_n) or (np.any(np.abs(t_stats) > tstat_threshold))
 
     if apply_shanken:
